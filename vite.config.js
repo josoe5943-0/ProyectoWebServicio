6a590439920c8c075f20f32d48b154335d1abd1c
josoe5943-0 <josegarciamelchor30@gmail.com>
2026-03-09 02:15:43 -0400

    primer commit web

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  base: '/mi-web/',
+  plugins: [react(), tailwindcss()],
+})
